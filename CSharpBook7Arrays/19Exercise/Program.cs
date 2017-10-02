14240963f77adf39be090cc8318cadec14ad55e6
MilenN <milennikolov_bg@yahoo.com>
2017-10-02 12:24:12 +0300

    Add Arrays 19 and 20

---
+++ CSharpBook7Arrays/19Exercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19Exercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			bool[] array = new bool[(int)Math.Sqrt(n)];
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = true;
+			}
+			for (int i = 2; i < array.Length; i++)
+			{
+				if (array[2] == true)
+				{
+					for (int j = i*i ; j < array.Length; j=j+i)
+					{
+						array[j] = false;
+					}
+				}
+			}
+			for (int m = 2; m < array.Length; m++)
+			{
+				if (array[m] == true)
+				{
+					Console.Write($"{m} ");
+				}
+			}
+
+		}
+	}
+}
---
+++ CSharpBook7Arrays/20Exercise/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20Exercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());   //array length
+			int s = int.Parse(Console.ReadLine());   //target sum
+			int[] array = Console.ReadLine().Split(' ').Select(str=>int.Parse(str)).ToArray();
+			int[] possibleSums = new int[n];         //array of possible sums
+			
+			for (int i = 0; i < n; i++)              //initialize array of possible sums same as array 
+			{
+				possibleSums[i] = array[i];
+			}
+
+			bool[] possible = new bool[n];           //bool array to keep track if the target sum is hit
+			int lastArrayIndex = -1;
+			int lastPossibleIndex = -1;
+
+			for (int i = 0; i < n; i++)              //nested loop to add any element of array to all other elements of possibleSums
+			{
+				for (int j = 0; j < n; j++)
+				{
+					if (j != i)
+					{
+						possibleSums[j] = possibleSums[j] + array[i];
+						if (possibleSums[j] == s)   //If target sum is hit, break loop and print 'Yes'
+						{
+							possible[j] = true;
+							lastPossibleIndex = j;
+							lastArrayIndex = i;
+							Console.Write("Yes -> (");
+							break;
+						}
+					}
+				}
+				if (lastPossibleIndex >= 0)
+				{
+					break;
+				}
+			}
+			if (lastPossibleIndex >=0)
+			{
+				for (int i = 0; i <= lastArrayIndex; i++)
+				{
+					if (i != lastPossibleIndex)
+					{
+						Console.Write($"{array[i]} + ");
+					}
+				}
+				Console.Write($"{array[lastPossibleIndex]})");
+			}
+			
+			}
+		}
+	}
+
